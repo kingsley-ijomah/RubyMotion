a2e02a37facdc139bc16ea8965e6a817cd93c6f9
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-06-23 17:01:59 +0100

    Add animation

--- Boxey/app/app_delegate.rb
+++ Boxey/app/app_delegate.rb
@@ -13,7 +13,7 @@ class AppDelegate
     @blue_view = UIView.alloc.initWithFrame(CGRect.new([10, 70], [100, 100]))
     @blue_view.backgroundColor = UIColor.blueColor
     @window.addSubview(@blue_view)
-    
+
     @add_button = UIButton.buttonWithType(UIButtonTypeSystem)
     @add_button.setTitle("Tap", forState: UIControlStateNormal)
     @add_button.layer.cornerRadius = 5
@@ -24,7 +24,42 @@ class AppDelegate
     @window.addSubview(@add_button)
     @add_button.addTarget(self, action:"add_tapped", forControlEvents:UIControlEventTouchUpInside)
 
+    @remove_button = UIButton.buttonWithType(UIButtonTypeSystem)
+    @remove_button.setTitle("Remove", forState:UIControlStateNormal)
+    @remove_button.sizeToFit
+    @remove_button.frame = CGRect.new(
+      [@add_button.frame.origin.x + @add_button.frame.size.width + 10,
+       @add_button.frame.origin.y],
+       @remove_button.frame.size)
+    @window.addSubview(@remove_button)
+    @remove_button.addTarget(self, action:"remove_tapped", forControlEvents:UIControlEventTouchUpInside)
+
     true
+  end
+
+  def remove_tapped
+    other_views = @window.subviews.select do |view|
+      not view.is_a? UIButton
+    end
+    @last_view = other_views.last
+
+    if @last_view and other_views.count > 1 
+      UIView.animateWithDuration(0.5,
+        animations:lambda {
+          @last_view.alpha = 0 
+          @last_view.backgroundColor = UIColor.redColor 
+          other_views.each do |view|
+            next if view == @last_view 
+            view.frame = CGRect.new(
+              [view.frame.origin.x,
+               view.frame.origin.y - (@last_view.frame.size.height + 10)],
+              view.frame.size)
+          end
+        },
+        completion:lambda { |finished|
+          @last_view.removeFromSuperview
+        })
+    end
   end
 
   def add_tapped
