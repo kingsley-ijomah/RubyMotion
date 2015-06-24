ba5825900fffe61eab6cfed0530c66e3cabd356a
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-06-24 14:52:41 +0100

    Intro into UINavigation container

--- ColorViewer/app/app_delegate.rb
+++ ColorViewer/app/app_delegate.rb
@@ -1,9 +1,11 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
-    @window.rootViewController = ColorsController.alloc.initWithNibName(nil, bundle: nil)
-
     @window.makeKeyAndVisible
+
+    controller = ColorsController.alloc.initWithNibName(nil, bundle: nil)
+    nav_controller = UINavigationController.alloc.initWithRootViewController(controller)
+    @window.rootViewController = nav_controller
 
     true
   end
