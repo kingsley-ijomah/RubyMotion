c7f1fee782c8209d62ec7c651693a6e09a3372a6
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-06-28 11:56:39 +0100

    Update test controller

--- UILabel/app/controllers/test_controller.rb
+++ UILabel/app/controllers/test_controller.rb
@@ -1,7 +1,16 @@
 class TestController < UIViewController
   def viewDidLoad
     super
-    self.view.backgroundColor = UIColor.blueColor
+    self.view.backgroundColor = UIColor.whiteColor
+    self.view.addSubview(label)
+  end
+
+  def label
+    label = UILabel.alloc.initWithFrame(CGRectZero)
+    label.text = 'Testing Labels'
+    label.sizeToFit
+    label.center = [self.view.frame.size.width / 2, self.view.frame.size.height / 2]
+    label
   end
 end
 
