4d470c6879ba6c3962427048b181a5ed858f89c9
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-06-30 14:04:08 +0100

    Add save and unload method to model

--- UserProfile/app/models/user.rb
+++ UserProfile/app/models/user.rb
@@ -1,6 +1,7 @@
 class User
   PROPERTIES = [:id, :name, :email, :phone]
   PROPERTIES.each { |prop| attr_accessor prop }
+  USER_KEY = "user"
 
   def initialize(properties = {})
     properties.each do |key, value|
@@ -8,5 +9,32 @@ class User
         self.send("#{key}=", value)
       end
     end
+  end
+
+  def save
+    defaults = NSUserDefaults.standardUserDefaults
+    defaults[USER_KEY] = NSKeyedArchiver.archivedDataWithRootObject(self)
+  end
+
+  def self.load
+    defaults = NSUserDefaults.standardUserDefaults
+    data = defaults[USER_KEY]
+    # protect against nil case 
+    NSKeyedUnarchiver.unarchiveObjectWithData(data) if data
+  end
+
+  def initWithCoder(decoder) 
+    self.init
+    PROPERTIES.each do |prop|
+      saved_value = decoder.decodeObjectForKey(prop.to_s)
+      self.send("#{prop}=", saved_value) 
+    end
+    self
+  end
+
+  def encodeWithCoder(encoder)
+    PROPERTIES.each do |prop| 
+      encoder.encodeObject(self.send(prop), forKey: prop.to_s)
+    end 
   end
 end
