dbf42d7a5a1d2048b29b7123b1ba0f8467e0f4db
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-06-30 14:26:42 +0100

    Add tabe

---
+++ TableFun/app/controllers/alphabet_controller.rb
@@ -0,0 +1,24 @@
+class AlphabetController < UIViewController
+  def viewDidLoad
+    super
+    self.title = 'Alphabet'
+    @table = UITableView.alloc.initWithFrame(self.view.bounds) 
+    @table.autoresizingMask = UIViewAutoresizingFlexibleHeight 
+    @table.dataSource = self #implements tableView
+    @data = ("A".."Z").to_a
+
+    self.view.addSubview(@table)
+  end
+
+  def tableView(tableView, numberOfRowsInSection: section) 
+    @data.count
+  end
+
+  def tableView(tableView, cellForRowAtIndexPath: indexPath)
+    @reuseIdentifier ||= "CELL_IDENTIFIER"
+    cell = tableView.dequeueReusableCellWithIdentifier(@reuseIdentifier)
+    cell ||= UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier:@reuseIdentifier)
+    cell.textLabel.text = @data[indexPath.row]
+    cell
+  end
+end
