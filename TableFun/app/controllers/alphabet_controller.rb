948f8310ebcd3ea2b7fe2492f359301b4d8ba8e5
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-07-01 16:56:32 +0100

    Add table scroller

--- TableFun/app/controllers/alphabet_controller.rb
+++ TableFun/app/controllers/alphabet_controller.rb
@@ -5,6 +5,7 @@ class AlphabetController < UIViewController
     @table = UITableView.alloc.initWithFrame(self.view.bounds) 
     @table.autoresizingMask = UIViewAutoresizingFlexibleHeight 
     @table.dataSource = self #implements tableView
+    @table.delegate = self #handles what to do after interacting with table
     @data = ("A".."Z").to_a
 
     self.view.addSubview(@table)
@@ -16,9 +17,33 @@ class AlphabetController < UIViewController
 
   def tableView(tableView, cellForRowAtIndexPath: indexPath)
     @reuseIdentifier ||= "CELL_IDENTIFIER"
-    cell = tableView.dequeueReusableCellWithIdentifier(@reuseIdentifier)
-    cell ||= UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier:@reuseIdentifier)
+    cell = tableView.dequeueReusableCellWithIdentifier(@reuseIdentifier) || begin
+      UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier:@reuseIdentifier)
+    end
+    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator
     cell.textLabel.text = @data[indexPath.row]
     cell
   end
+
+  def tableView(tableView, didSelectRowAtIndexPath:indexPath)
+    tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    letter = @data[indexPath.row]
+
+    controller = UIViewController.alloc.initWithNibName(nil, bundle:nil)
+    controller.view.backgroundColor = UIColor.whiteColor
+    controller.title = letter
+    label = UILabel.alloc.initWithFrame(CGRectZero)
+
+    label.text = letter
+    label.sizeToFit
+    label.center = [controller.view.frame.size.width / 2,
+                    controller.view.frame.size.height / 2]
+    controller.view.addSubview(label)
+    self.navigationController.pushViewController(controller, animated:true)
+  end
 end
+
+
+
+
+
