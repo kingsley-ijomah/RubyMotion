6d9688275d7f55f20b239686e447b5f28077f98c
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-07-02 10:27:59 +0100

    Testing parameters

--- Hello/app/app_delegate.rb
+++ Hello/app/app_delegate.rb
@@ -1,15 +1,19 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
 
-    alert = UIAlertView.alloc.initWithTitle("RubyMotion", message: "RubyMotion is awesome!", delegate: self,
-                                            cancelButtonTitle: "OK",
+    @alert = UIAlertView.alloc.initWithTitle("RubyMotion", 
+                                            message: "RubyMotion is awesome!", 
+                                            delegate: self,
+                                            cancelButtonTitle: "Cancel",
                                             otherButtonTitles: nil)
-    alert.show
-
+    @alert.addButtonWithTitle 'Sign in'
+    @alert.alertViewStyle = UIAlertViewStyleLoginAndPasswordInput
+    @alert.show
     true
   end
 
   def alertView(alertView, clickedButtonAtIndex: indexPath)
-    puts 'clicked button'
+    puts @alert.numberOfButtons
+    puts @alert.buttonTitleAtIndex indexPath
   end
 end
