80f4891b45610aeb69a0a6ab566b746530534277
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-07-02 10:28:11 +0100

    Add sections

--- TableFun/app/controllers/alphabet_controller.rb
+++ TableFun/app/controllers/alphabet_controller.rb
@@ -6,42 +6,76 @@ class AlphabetController < UIViewController
     @table.autoresizingMask = UIViewAutoresizingFlexibleHeight 
     @table.dataSource = self #implements tableView
     @table.delegate = self #handles what to do after interacting with table
-    @data = ("A".."Z").to_a
+    @data = {}
+    ("A".."Z").to_a.each do |letter|
+      @data[letter] = [] 
+      5.times do
+        random_string = (0...4).map{65.+(rand(25)).chr}.join
+        @data[letter] << letter + random_string
+      end 
+    end
 
     self.view.addSubview(@table)
   end
 
   def tableView(tableView, numberOfRowsInSection: section) 
-    @data.count
+    rows_for_section(section).count
   end
 
   def tableView(tableView, cellForRowAtIndexPath: indexPath)
     @reuseIdentifier ||= "CELL_IDENTIFIER"
     cell = tableView.dequeueReusableCellWithIdentifier(@reuseIdentifier) || begin
-      UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier:@reuseIdentifier)
+    UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier:@reuseIdentifier)
     end
     cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator
-    cell.textLabel.text = @data[indexPath.row]
+    cell.textLabel.text = row_for_index_path(indexPath)
     cell
   end
 
   def tableView(tableView, didSelectRowAtIndexPath:indexPath)
     tableView.deselectRowAtIndexPath(indexPath, animated: true)
-    letter = @data[indexPath.row]
+    letter = sections[indexPath.section]
 
     controller = UIViewController.alloc.initWithNibName(nil, bundle:nil)
     controller.view.backgroundColor = UIColor.whiteColor
     controller.title = letter
     label = UILabel.alloc.initWithFrame(CGRectZero)
 
-    label.text = letter
+    label.text = row_for_index_path(indexPath)
     label.sizeToFit
     label.center = [controller.view.frame.size.width / 2,
                     controller.view.frame.size.height / 2]
     controller.view.addSubview(label)
     self.navigationController.pushViewController(controller, animated:true)
   end
+
+  def sections
+    @data.keys.sort
+  end
+
+  def rows_for_section(section_index) 
+    @data[self.sections[section_index]]
+  end
+
+  def row_for_index_path(index_path) 
+    rows_for_section(index_path.section)[index_path.row]
+  end
+
+  def numberOfSectionsInTableView(tableView) 
+    self.sections.count
+  end
+
+  def tableView(tableView, titleForHeaderInSection:section) 
+    sections[section]
+  end
+
+
 end
+
+
+
+
+
 
 
 
