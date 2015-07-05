053052d5ec36fc1942860b01eeafb656374fe0a8
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-07-05 15:33:03 +0100

    Attempt hooking up interface builder

--- Paint/app/app_delegate.rb
+++ Paint/app/app_delegate.rb
@@ -1,15 +1,11 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
-    rootViewController = UIViewController.alloc.init
-    rootViewController.title = 'Paint'
-    rootViewController.view.backgroundColor = UIColor.yellowColor
-
-    navigationController = UINavigationController.alloc.initWithRootViewController(rootViewController)
-
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
-    @window.rootViewController = navigationController
-    @window.makeKeyAndVisible
 
+    @storyboard ||= UIStoryboard.storyboardWithName('MainStoryboard', bundle:NSBundle.mainBundle)
+    @window.rootViewController = @storyboard.instantiateInitialViewController
+
+    @window.makeKeyAndVisible
     true
   end
 end
