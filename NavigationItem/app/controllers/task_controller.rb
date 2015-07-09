212fc98705a6808dc0fa597f3f0045cef27aa7a9
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-07-09 12:30:56 +0100

    Add task controller

--- NavigationItem/app/controllers/task_controller.rb
+++ NavigationItem/app/controllers/task_controller.rb
@@ -1,3 +1,13 @@
 class TaskController < UIViewController
 
+  def viewWillAppear(animated)
+    self.view.backgroundColor = UIColor.whiteColor
+    navigationItem.title = "My Task List"
+    navigationItem.leftBarButtonItem = editButtonItem
+    navigationItem.rightBarButtonItem = UIBarButtonItem.alloc.initWithBarButtonSystemItem(UIBarButtonSystemItemAdd, target:self, action:'testMe')
+  end
+
+  def testMe
+    puts 'I was clicked'
+  end
 end
