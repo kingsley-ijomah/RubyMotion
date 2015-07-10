d700a08d720ebfaeb771cd41f7d1f24c5159b2d6
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-07-10 11:47:54 +0100

    Create form controller for todo

---
+++ Todo/app/controllers/form_controller.rb
@@ -0,0 +1,10 @@
+class FormController < Formotion::FormController
+  attr_accessor :todo
+  attr_accessor :form
+
+  def initialize(todo)
+    self.form = Formotion::Form.new(todo.to_formotion('Edit your ToDo'))
+    self.initWithForm(self.form)
+    self.todo = todo
+  end
+end
